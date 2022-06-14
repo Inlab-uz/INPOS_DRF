d286b276dec0a85c51b5aab4539f6c7c809ecb4f
Asadbek <solijonovasadbek25111999@gmail.com>
2022-06-14 23:14:03 +0500

    Redoc and Swagger Docs

--- product/urls.py
+++ product/urls.py
@@ -1,18 +1,34 @@
 from django.db import router
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
-
 from product.views import CategoryViewSet, ProductViewSet
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('categorys', CategoryViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='IN Point of Sale Django Rest Framework API',
+        default_version='v1',
+        description='This is INPOS system API Docs',
+        terms_of_service='',
+        contact=openapi.Contact(email="Solijonov Asadbek <solijonovasadbek25111999@gmail.com>"),
+        license=openapi.License(name='')
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', obtain_auth_token),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-docs'),
 ]
 
 from product import views
